fa9e9be2add22a44ab0b344df48d72f8cee4b769
mohammedaitgaba <93977101+mohammedaitgaba@users.noreply.github.com>
2023-02-20 09:30:24 +0100

    add BottomTabNavigator

--- MobileEScooter/App.js
+++ MobileEScooter/App.js
@@ -1,34 +1,21 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View} from "react-native";
-import Home from './Screens/Home'
-import LoginScreen from "./Screens/Login";
-import RegisterScreen from './Screens/Register'
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
+
+
+// import Home from './Screens/Home'
+// import LoginScreen from "./Screens/Login";
+// import RegisterScreen from './Screens/Register'
+
+import MainContainer from "./Navigation/MainContainer";
 
 const Stack = createNativeStackNavigator();
 
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen
-          name="Login"
-          component={LoginScreen}
-          options=
-          {{
-            title: 'Welcome',
-            headerStyle: { backgroundColor: 'white' },
-            headerTintColor: 'blue',
-            headerTitleStyle: { fontWeight: 'bold' },
-            headerTitleAlign:'center'
-          }}
-        />
-        <Stack.Screen name="Home" component={Home} />
-        <Stack.Screen name="Register" component={RegisterScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <MainContainer/>
   );
 }
 
